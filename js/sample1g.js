d319723f11c4595aabd59634340260b3c1757013
alex <alex_samarkin@mail.ru>
2018-08-08 00:55:21 +0300

    2

---
+++ js/sample1g.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    id: 1,
+    name: "Аспирин",
+    fullName: "Ацетилсалициловая кислота",
+    latName: "Acidum acetylsalicylicum",
+    count: 120,
+    cost: 18.3,
+    inStock: true
+  },
+  computed: {
+    overall: function() {
+      return this.count * this.cost;
+    }
+  }
+});
---
+++ js/sample2.js
@@ -0,0 +1,38 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        medicaments: [{
+                id: 1,
+                name: "Аспирин",
+                fullName: "Ацетилсалициловая кислота",
+                latName: "Acidum acetylsalicylicum",
+                count: 120,
+                cost: 18.3,
+                inStock: true
+            },
+            {
+                id: 2,
+                name: "Анальгин",
+                fullName: "Метамизол натрия",
+                latName: "Metamizol natrii",
+                count: 62,
+                cost: 25.8,
+                inStock: true
+            },
+            {
+                id: 3,
+                name: "Найз",
+                fullName: "Нимесулид",
+                latName: "-",
+                count: 6,
+                cost: 150.2,
+                inStock: false
+            }
+        ]
+    },
+    computed: {
+        overall: function () {
+            return this.count * this.cost;
+        }
+    }
+});
