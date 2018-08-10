2ca13edc782a3a5b4af49429928c8ed1a9d9ad65
alex <alex_samarkin@mail.ru>
2018-08-11 01:47:26 +0300

    2a

--- js/sample2a.js
+++ js/sample2a.js
@@ -1,46 +1,47 @@
 var app = new Vue({
-    el: "#app",
-    data: {
+  el: "#app",
+  data: {
+    id: 1,
+    name: "Аспирин",
+    fullName: "Ацетилсалициловая кислота",
+    latName: "Acidum acetylsalicylicum",
+    count: 120,
+    cost: 18.3,
+    inStock: true,
+
+    medics: [
+      {
         id: 1,
         name: "Аспирин",
         fullName: "Ацетилсалициловая кислота",
         latName: "Acidum acetylsalicylicum",
         count: 120,
         cost: 18.3,
-        inStock: true,
-
-        medics: [{
-                id: 1,
-                name: "Аспирин",
-                fullName: "Ацетилсалициловая кислота",
-                latName: "Acidum acetylsalicylicum",
-                count: 120,
-                cost: 18.3,
-                inStock: true
-            },
-            {
-                id: 2,
-                name: "Анальгин",
-                fullName: "Метамизол натрия",
-                latName: "Metamizol natrii",
-                count: 62,
-                cost: 25.8,
-                inStock: true
-            },
-            {
-                id: 3,
-                name: "Найз",
-                fullName: "Нимесулид",
-                latName: "-",
-                count: 6,
-                cost: 150.2,
-                inStock: false
-            }
-        ]
-    },
-    computed: {
-        overall: function () {
-            return this.count * this.cost;
-        }
+        inStock: true
+      },
+      {
+        id: 2,
+        name: "Анальгин",
+        fullName: "Метамизол натрия",
+        latName: "Metamizol natrii",
+        count: 62,
+        cost: 25.8,
+        inStock: true
+      },
+      {
+        id: 3,
+        name: "Найз",
+        fullName: "Нимесулид",
+        latName: "-",
+        count: 6,
+        cost: 150.2,
+        inStock: false
+      }
+    ]
+  },
+  computed: {
+    overall: function() {
+      return this.count * this.cost;
     }
+  }
 });
---
+++ js/sample2b.js
@@ -0,0 +1,49 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        id: 1,
+        name: "Аспирин",
+        fullName: "Ацетилсалициловая кислота",
+        latName: "Acidum acetylsalicylicum",
+        count: 120,
+        cost: 18.3,
+        inStock: true,
+
+        medics: [{
+                id: 1,
+                name: "Аспирин",
+                fullName: "Ацетилсалициловая кислота",
+                latName: "Acidum acetylsalicylicum",
+                count: 120,
+                cost: 18.3,
+                inStock: true
+            },
+            {
+                id: 2,
+                name: "Анальгин",
+                fullName: "Метамизол натрия",
+                latName: "Metamizol natrii",
+                count: 62,
+                cost: 25.8,
+                inStock: true
+            },
+            {
+                id: 3,
+                name: "Найз",
+                fullName: "Нимесулид",
+                latName: "-",
+                count: 6,
+                cost: 150.2,
+                inStock: false
+            }
+        ]
+    },
+    computed: {
+        overall: function () {
+            return this.count * this.cost;
+        },
+        overall2: function (index) {
+            return (this.medics[index]).cost * (this.medics[index]).count;
+        }
+    }
+});
